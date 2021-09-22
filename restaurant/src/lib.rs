503b3fa8684243cc751c0e38f1467599083c71b2
Fomegne Meudje <fomegnemeudje@outlook.com>
2021-09-22 16:21:05 +0800

    new

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,27 @@
+mod front_of_house {
+	pub mod hosting {
+		pub fn add_to_waitlist() {}
+		fn seat_at_table() {}
+	}
+	mod serving {
+		fn take_order() {}
+		fn serve_order() {}
+		fn take_payment() {}
+	}
+}
+
+mod back_of_house {
+	fn fix_incorrect_order() {
+		cook_order();
+		super::front_of_house::serving::serve_order();
+	}
+	fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+	// Absolute path
+	crate::front_of_house::hosting::add_to_waitlist();
+	
+	// Relative path
+	front_of_house::hosting::add_to_waitlist();
+}
